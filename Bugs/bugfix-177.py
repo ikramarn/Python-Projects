d0d48b9868bef53f77ba427fd5384fe4a5df9318
ikramarn <ikramarn@gmail.com>
2024-11-06 17:35:52 +0000

    adding match and exit and value error

---
+++ Bugs/bugfix-177.py
@@ -0,0 +1,45 @@
+import os
+os.environ['TCL_LIBRARY'] = r'C:\Users\User1\AppData\Local\Programs\Python\Python313\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\User1\AppData\Local\Programs\Python\Python313\tcl\tk8.6'
+
+import FreeSimpleGUI as sg
+
+
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
+
+
+sg.theme("Black")
+
+feet_label = sg.Text("Enter feet: ")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches: ")
+inches_input = sg.Input(key="inches")
+
+button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+exit_button = sg.Button("Exit")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [button, exit_button, output_label]])
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+    try:
+        feet = float(values["feet"])
+        inches = float(values["inches"])
+        result = convert(feet, inches)
+        window["output"].update(value=f"{result} m", text_color="white")
+    except ValueError:
+        sg.popup("please enter values")
+
+window.close()
--- Exercices/, 177.py
+++ Exercices/, 177.py
@@ -12,14 +12,22 @@ input1 = sg.Input()
 label2 = sg.Text("Enter Inches")
 input2 = sg.Input()
 button = sg.Button("Convert")
+button2 = sg.Button("Exit")
 output_text = sg.Text(key="output",text_color= "green")
-window = sg.Window("Converter",[[label, input1],[label2,input2],[button,output_text]])
-while True:
-    data = window.read()
-    data = list(data)
-    feet = float(data[1][0])
-    inches = float(data[1][1])
-    Meters=(meters(feet,inches))
-    window["output"].update(value=f"{Meters}")
 
-window.close()
+window = sg.Window("Converter",[[label, input1],[label2,input2],[button,button2,output_text]])
+
+while True:
+
+    event, data = window.read()
+    print(event)
+    print(data)
+    match event:
+        case "Convert":
+            feet = float(data[0])
+            inches = float(data[1])
+            Meters=(meters(feet,inches))
+            window["output"].update(value=f"{Meters}")
+
+        case "Exit":
+            break
